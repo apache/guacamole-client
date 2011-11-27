e8f83c19ccb07c804330696358d4985b0b5b3465
Michael Jumper <zhangmaike@users.sourceforge.net>
2011-11-26 18:23:45 -0800

    Renamed and reformatted example, removed unapplicable license terms.

--- guacamole-common/doc/example/ExampleTunnelServlet.java
+++ guacamole-common/doc/example/ExampleTunnelServlet.java
@@ -1,6 +1,4 @@
 
-package net.sourceforge.guacamole.net.example;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import net.sourceforge.guacamole.GuacamoleException;
@@ -13,33 +11,19 @@ import net.sourceforge.guacamole.protocol.ConfiguredGuacamoleSocket;
 import net.sourceforge.guacamole.servlet.GuacamoleSession;
 import net.sourceforge.guacamole.servlet.GuacamoleTunnelServlet;
 
-/*
- *  Guacamole - Clientless Remote Desktop
- *  Copyright (C) 2010  Michael Jumper
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-public class DummyGuacamoleTunnelServlet extends GuacamoleTunnelServlet {
+public class ExampleTunnelServlet extends GuacamoleTunnelServlet {
 
     @Override
-    protected GuacamoleTunnel doConnect(HttpServletRequest request) throws GuacamoleException {
+    protected GuacamoleTunnel doConnect(HttpServletRequest request)
+        throws GuacamoleException {
 
         HttpSession httpSession = request.getSession(true);
 
-        String hostname = GuacamoleProperties.getProperty(GuacamoleProperties.GUACD_HOSTNAME);
-        int port = GuacamoleProperties.getProperty(GuacamoleProperties.GUACD_PORT);
+        String hostname = GuacamoleProperties.getProperty(
+            GuacamoleProperties.GUACD_HOSTNAME);
+
+        int port = GuacamoleProperties.getProperty(
+                GuacamoleProperties.GUACD_PORT);
 
         GuacamoleConfiguration config = new GuacamoleConfiguration();
         config.setProtocol("vnc");
