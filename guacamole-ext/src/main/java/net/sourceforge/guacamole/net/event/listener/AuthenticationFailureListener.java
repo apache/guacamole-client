e9fcab10b189fc7aef3627b1780547e7dc73cf82
Michael Jumper <zhangmaike@users.sourceforge.net>
2012-03-23 11:54:18 -0700

    Renamed hook function.

--- guacamole-ext/src/main/java/net/sourceforge/guacamole/net/event/listener/AuthenticationFailureListener.java
+++ guacamole-ext/src/main/java/net/sourceforge/guacamole/net/event/listener/AuthenticationFailureListener.java
@@ -3,10 +3,9 @@ package net.sourceforge.guacamole.net.event.listener;
 import net.sourceforge.guacamole.net.event.AuthenticationFailureEvent;
 
 /**
- * A listener whose postAuthenticationFailure() hook will fire immediately
- * after a user's authentication attempt fails. Note that there is no
- * preAuthenticationFailure() hook - authentication failure cannot be canceled,
- * it can only be observed after the fact.
+ * A listener whose authenticationFailed() hook will fire immediately
+ * after a user's authentication attempt fails. Note that this hook cannot
+ * be used to cancel the authentication failure.
  * 
  * @author Michael Jumper
  */
@@ -19,6 +18,6 @@ public interface AuthenticationFailureListener  {
      * @param e The AuthenticationFailureEvent describing the authentication
      *          failure that just occurred.
      */
-    public void postAuthenticationFailure(AuthenticationFailureEvent e);
+    public void authenticationFailed(AuthenticationFailureEvent e);
     
 }
