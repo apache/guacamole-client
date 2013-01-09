b7071563279dc12f3643f9d3e986f022dffdd1c9
Michael Jumper <zhangmaike@users.sourceforge.net>
2013-01-09 15:18:35 -0800

    Fix typo in variable name.

--- guacamole-common-js/src/main/resources/keyboard.js
+++ guacamole-common-js/src/main/resources/keyboard.js
@@ -318,7 +318,7 @@ Guacamole.Keyboard = function(element) {
 
     }
 
-    function keysyom_from_keycode(keyCode) {
+    function keysym_from_keycode(keyCode) {
 
         var keysym = null;
         if (!guac_keyboard.modifiers.shift) keysym = unshiftedKeysym[keyCode];
@@ -437,7 +437,7 @@ Guacamole.Keyboard = function(element) {
         else if (keynum == 18) guac_keyboard.modifiers.alt   = true;
 
         // Try to get keysym from keycode
-        var keysym = keysyom_from_keycode(keynum);
+        var keysym = keysym_from_keycode(keynum);
 
         // By default, we expect a corresponding keypress event
         var expect_keypress = true;
