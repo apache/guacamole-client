f5529376b6bc2227bf5e03705dd4eaafb0d3f6dd
James Muehlner <james.muehlner@guac-dev.org>
2013-02-01 00:33:38 -0800

    Ticket #269: Cleaning up default values and null constraints.

--- extensions/guacamole-auth-mysql/schema/guacamole-auth-mysql-schema.sql
+++ extensions/guacamole-auth-mysql/schema/guacamole-auth-mysql-schema.sql
@@ -28,9 +28,9 @@ CREATE TABLE `guacamole_user` (
 --
 
 CREATE TABLE `guacamole_connection_parameter` (
-  `connection_id` int(11) NOT NULL DEFAULT '0',
-  `parameter_name` varchar(128) NOT NULL DEFAULT '',
-  `parameter_value` varchar(4096) DEFAULT NULL,
+  `connection_id` int(11) NOT NULL,
+  `parameter_name` varchar(128) NOT NULL,
+  `parameter_value` varchar(4096) NOT NULL,
   PRIMARY KEY (`connection_id`,`parameter_name`),
   CONSTRAINT `guacamole_connection_parameter_ibfk_1` FOREIGN KEY (`connection_id`) REFERENCES `guacamole_connection` (`connection_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -40,8 +40,8 @@ CREATE TABLE `guacamole_connection_parameter` (
 --
 
 CREATE TABLE `guacamole_connection_permission` (
-  `user_id` int(11) NOT NULL DEFAULT '0',
-  `connection_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `connection_id` int(11) NOT NULL,
   `permission` enum('READ','WRITE','DELETE','ADMINISTER') NOT NULL,
   PRIMARY KEY (`user_id`,`connection_id`,`permission`),
   CONSTRAINT `guacamole_connection_permission_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `guacamole_connection` (`connection_id`),
@@ -53,7 +53,7 @@ CREATE TABLE `guacamole_connection_permission` (
 --
 
 CREATE TABLE `guacamole_system_permission` (
-  `user_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
   `permission` enum('CREATE_CONFIGURATION','CREATE_USER') NOT NULL,
   PRIMARY KEY (`user_id`,`permission`),
   CONSTRAINT `guacamole_system_permission_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `guacamole_user` (`user_id`)
@@ -64,8 +64,8 @@ CREATE TABLE `guacamole_system_permission` (
 --
 
 CREATE TABLE `guacamole_user_permission` (
-  `user_id` int(11) NOT NULL DEFAULT '0',
-  `affected_user_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `affected_user_id` int(11) NOT NULL,
   `permission` enum('READ','WRITE','DELETE','ADMINISTER') NOT NULL,
   PRIMARY KEY (`user_id`,`affected_user_id`,`permission`),
   CONSTRAINT `guacamole_user_permission_ibfk_1` FOREIGN KEY (`affected_user_id`) REFERENCES `guacamole_user` (`user_id`),
