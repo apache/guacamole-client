e493b36c62cfdad19de283f8d44e8554830baee1
Michael Jumper <mike.jumper@guac-dev.org>
2013-02-09 01:54:35 -0800

    #268: Add create/delete/update stubs.

---
+++ guacamole/src/main/java/net/sourceforge/guacamole/net/basic/crud/connections/Delete.java
@@ -0,0 +1,46 @@
+package net.sourceforge.guacamole.net.basic.crud.connections;
+
+/*
+ *  Guacamole - Clientless Remote Desktop
+ *  Copyright (C) 2010  Michael Jumper
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sourceforge.guacamole.net.auth.UserContext;
+import net.sourceforge.guacamole.net.basic.AuthenticatingHttpServlet;
+
+/**
+ * Simple HttpServlet which handles connection deletion.
+ *
+ * @author Michael Jumper
+ */
+public class Delete extends AuthenticatingHttpServlet {
+
+    @Override
+    protected void authenticatedService(
+            UserContext context,
+            HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+
+        /* FIXME: STUB */
+        
+    }
+
+}
+
