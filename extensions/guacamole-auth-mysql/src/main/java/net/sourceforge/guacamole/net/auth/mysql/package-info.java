f8af9da4bd40ae17af2ad7cf6c6bc72df87f0b4e
Michael Jumper <mike.jumper@guac-dev.org>
2013-02-23 00:43:52 -0800

    Ticket #269: Add package-info.java files.

---
+++ extensions/guacamole-auth-mysql/src/main/java/net/sourceforge/guacamole/net/auth/mysql/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * Base classes which support the MySQL authentication provider, including
+ * the authentication provider itself.
+ */
+package net.sourceforge.guacamole.net.auth.mysql;
+
---
+++ extensions/guacamole-auth-mysql/src/main/java/net/sourceforge/guacamole/net/auth/mysql/utility/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * Service classes which help fill the needs of the MySQL authentication
+ * provider.
+ */
+package net.sourceforge.guacamole.net.auth.mysql.utility;
+
