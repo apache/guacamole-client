79872b9012011738bb173d0a89a46159ba7ca13b
James Muehlner <james.muehlner@guac-dev.org>
2013-02-26 21:13:49 -0800

    Ticket #269: Fixed password initialization in create user script, and removed hard-coded database names in XML mapping files.

--- extensions/guacamole-auth-mysql/schema/guacamole-auth-mysql-create-default-user.sql
+++ extensions/guacamole-auth-mysql/schema/guacamole-auth-mysql-create-default-user.sql
@@ -1,7 +1,7 @@
 
 -- Create default user "guacadmin" with password "guacadmin"
 insert into guacamole_user values(1, 'guacadmin',
-    x'AE97B20D5B24B2F18BE7921E3C0CF6109696391D7D5A6BE24BD267E49F0D7E42',  -- 'guacadmin'
+    x'CA458A7D494E3BE824F5E1E175A1556C0F8EEF2C2D7DF3633BEC4A29C4411960',  -- 'guacadmin'
     x'FE24ADC5E11E2B25288D1704ABE67A79E342ECC26064CE69C5B3177795A82264');
 
 -- Grant this user create permissions
