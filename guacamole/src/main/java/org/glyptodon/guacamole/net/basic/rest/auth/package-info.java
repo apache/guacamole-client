0fefe94b4d9aa449668f477b3538e759aba14c29
James Muehlner <james.muehlner@guac-dev.org>
2013-10-02 21:12:17 -0700

    Ticket #362: Created packing info file.

--- guacamole/src/main/java/org/glyptodon/guacamole/net/basic/rest/auth/package-info.java
+++ guacamole/src/main/java/org/glyptodon/guacamole/net/basic/rest/auth/package-info.java
@@ -2,5 +2,5 @@
 /**
  * Classes related to the authentication aspect of the Guacamole REST API.
  */
-package org.glyptodon.guacamole.net.basic.rest;
+package org.glyptodon.guacamole.net.basic.rest.auth;
 
