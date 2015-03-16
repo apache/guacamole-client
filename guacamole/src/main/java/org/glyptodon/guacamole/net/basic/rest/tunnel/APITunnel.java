3a4f6b85dd97a6c7e725718472d3ad0df7295b29
Michael Jumper <mike.jumper@guac-dev.org>
2015-03-16 15:57:18 -0700

    GUAC-1132: Add tunnel rest service stub.

---
+++ guacamole/src/main/java/org/glyptodon/guacamole/net/basic/rest/tunnel/APITunnel.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2015 Glyptodon LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.glyptodon.guacamole.net.basic.rest.tunnel;
+
+import java.util.Date;
+import org.glyptodon.guacamole.net.auth.ConnectionRecord;
+
+/**
+ * Tunnel-related information which may be exposed through the REST endpoints.
+ * 
+ * @author Michael Jumper
+ */
+public class APITunnel {
+
+    /**
+     * The date and time the connection began.
+     */
+    private final Date startDate;
+
+    /**
+     * The host from which the connection originated, if known.
+     */
+    private final String remoteHost;
+    
+    /**
+     * The name of the user who used or is using the connection.
+     */
+    private final String username;
+
+    /**
+     * The UUID of the tunnel.
+     */
+    private final String uuid;
+    
+    /**
+     * Creates a new APITunnel, copying the information from the given
+     * connection record.
+     *
+     * @param record
+     *     The record to copy data from.
+     */
+    public APITunnel(ConnectionRecord record) {
+        this.startDate  = record.getStartDate();
+        this.remoteHost = record.getRemoteHost();
+        this.username   = record.getUsername();
+        this.uuid       = "STUB"; // STUB
+    }
+
+    /**
+     * Returns the date and time the connection began.
+     *
+     * @return
+     *     The date and time the connection began.
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Returns the remote host from which this connection originated.
+     *
+     * @return
+     *     The remote host from which this connection originated.
+     */
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    /**
+     * Returns the name of the user who used or is using the connection at the
+     * times given by this tunnel.
+     *
+     * @return
+     *     The name of the user who used or is using the associated connection.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Returns the UUID of the underlying Guacamole tunnel. Absolutely every
+     * Guacamole tunnel has an associated UUID.
+     *
+     * @return
+     *     The UUID of the underlying Guacamole tunnel.
+     */
+    public String getUUID() {
+        return uuid;
+    }
+    
+}
---
+++ guacamole/src/main/webapp/app/rest/types/ActiveTunnel.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 Glyptodon LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Service which defines the ActiveTunnel class.
+ */
+angular.module('rest').factory('ActiveTunnel', [function defineActiveTunnel() {
+            
+    /**
+     * The object returned by REST API calls when representing the data
+     * associated with an active tunnel. Each tunnel denotes an active
+     * connection, uniquely identified by the tunnel UUID.
+     * 
+     * @constructor
+     * @param {ActiveTunnel|Object} [template={}]
+     *     The object whose properties should be copied within the new
+     *     ActiveTunnel.
+     */
+    var ActiveTunnel = function ActiveTunnel(template) {
+
+        // Use empty object by default
+        template = template || {};
+
+        /**
+         * The time that the tunnel began, in seconds since
+         * 1970-01-01 00:00:00 UTC.
+         *
+         * @type Number 
+         */
+        this.startDate = template.startDate;
+
+        /**
+         * The remote host that initiated the tunnel, if known.
+         *
+         * @type String
+         */
+        this.remoteHost = template.remoteHost;
+
+        /**
+         * The username of the user associated with the tunnel.
+         * 
+         * @type String
+         */
+        this.username = template.username;
+
+        /**
+         * The UUID which uniquely identifies the tunnel.
+         * 
+         * @type String
+         */
+        this.uuid = template.uuid;
+
+    };
+
+    return ActiveTunnel;
+
+}]);
