55fb19c87be5e2aa4f6de18a201fc27e74fe76fe
Michael Jumper <mike.jumper@guac-dev.org>
2015-03-20 16:19:06 -0700

    GUAC-1132: Extract non-database pieces of DirectoryObjectService into IdentifiableObjectService interface.

--- extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/glyptodon/guacamole/auth/jdbc/base/DirectoryObjectService.java
+++ extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/glyptodon/guacamole/auth/jdbc/base/DirectoryObjectService.java
@@ -54,7 +54,8 @@ import org.glyptodon.guacamole.net.auth.permission.ObjectPermissionSet;
  *     database.
  */
 public abstract class DirectoryObjectService<InternalType extends DirectoryObject<ModelType>,
-        ExternalType, ModelType extends ObjectModel> {
+        ExternalType, ModelType extends ObjectModel>
+    implements IdentifiableObjectService<InternalType, ExternalType> {
 
     /**
      * All object permissions which are implicitly granted upon creation to the
@@ -289,23 +290,7 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
 
     }
 
-    /**
-     * Retrieves the single object that has the given identifier, if it exists
-     * and the user has permission to read it.
-     *
-     * @param user
-     *     The user retrieving the object.
-     *
-     * @param identifier
-     *     The identifier of the object to retrieve.
-     *
-     * @return
-     *     The object having the given identifier, or null if no such object
-     *     exists.
-     *
-     * @throws GuacamoleException
-     *     If an error occurs while retrieving the requested object.
-     */
+    @Override
     public InternalType retrieveObject(AuthenticatedUser user,
             String identifier) throws GuacamoleException {
 
@@ -324,23 +309,8 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
         return objects.iterator().next();
 
     }
-    
-    /**
-     * Retrieves all objects that have the identifiers in the given collection.
-     * Only objects that the user has permission to read will be returned.
-     *
-     * @param user
-     *     The user retrieving the objects.
-     *
-     * @param identifiers
-     *     The identifiers of the objects to retrieve.
-     *
-     * @return
-     *     The objects having the given identifiers.
-     *
-     * @throws GuacamoleException
-     *     If an error occurs while retrieving the requested objects.
-     */
+
+    @Override
     public Collection<InternalType> retrieveObjects(AuthenticatedUser user,
             Collection<String> identifiers) throws GuacamoleException {
 
@@ -403,25 +373,8 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
         return implicitPermissions;
 
     }
-    
-    /**
-     * Creates the given object within the database. If the object already
-     * exists, an error will be thrown. The internal model object will be
-     * updated appropriately to contain the new database ID.
-     *
-     * @param user
-     *     The user creating the object.
-     *
-     * @param object
-     *     The object to create.
-     *
-     * @return
-     *     The newly-created object.
-     *
-     * @throws GuacamoleException
-     *     If the user lacks permission to create the object, or an error
-     *     occurs while creating the object.
-     */
+
+    @Override
     public InternalType createObject(AuthenticatedUser user, ExternalType object)
         throws GuacamoleException {
 
@@ -438,20 +391,7 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
 
     }
 
-    /**
-     * Deletes the object having the given identifier. If no such object
-     * exists, this function has no effect.
-     *
-     * @param user
-     *     The user deleting the object.
-     *
-     * @param identifier
-     *     The identifier of the object to delete.
-     *
-     * @throws GuacamoleException
-     *     If the user lacks permission to delete the object, or an error
-     *     occurs while deleting the object.
-     */
+    @Override
     public void deleteObject(AuthenticatedUser user, String identifier)
         throws GuacamoleException {
 
@@ -462,20 +402,7 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
 
     }
 
-    /**
-     * Updates the given object in the database, applying any changes that have
-     * been made. If no such object exists, this function has no effect.
-     *
-     * @param user
-     *     The user updating the object.
-     *
-     * @param object
-     *     The object to update.
-     *
-     * @throws GuacamoleException
-     *     If the user lacks permission to update the object, or an error
-     *     occurs while updating the object.
-     */
+    @Override
     public void updateObject(AuthenticatedUser user, InternalType object)
         throws GuacamoleException {
 
@@ -487,19 +414,7 @@ public abstract class DirectoryObjectService<InternalType extends DirectoryObjec
 
     }
 
-    /**
-     * Returns the set of all identifiers for all objects in the database that
-     * the user has read access to.
-     *
-     * @param user
-     *     The user retrieving the identifiers.
-     *
-     * @return
-     *     The set of all identifiers for all objects in the database.
-     *
-     * @throws GuacamoleException
-     *     If an error occurs while reading identifiers.
-     */
+    @Override
     public Set<String> getIdentifiers(AuthenticatedUser user)
         throws GuacamoleException {
 
