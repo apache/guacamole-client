0b8b67026a4ed8bb2ee4b5357519c9930100409a
Michael Jumper <mike.jumper@guac-dev.org>
2015-06-08 01:44:53 -0700

    GUAC-1176: Implement password reset logic within UserService, like the rest of user auth.

--- extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/glyptodon/guacamole/auth/jdbc/user/UserContextService.java
+++ extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/src/main/java/org/glyptodon/guacamole/auth/jdbc/user/UserContextService.java
@@ -24,17 +24,10 @@ package org.glyptodon.guacamole.auth.jdbc.user;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import java.util.Arrays;
-import javax.servlet.http.HttpServletRequest;
-import org.glyptodon.guacamole.GuacamoleClientException;
 import org.glyptodon.guacamole.GuacamoleException;
-import org.glyptodon.guacamole.form.Field;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.credentials.CredentialsInfo;
-import org.glyptodon.guacamole.net.auth.credentials.GuacamoleInsufficientCredentialsException;
 import org.glyptodon.guacamole.net.auth.credentials.GuacamoleInvalidCredentialsException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Service which creates new UserContext instances for valid users based on
@@ -43,11 +36,6 @@ import org.slf4j.LoggerFactory;
  * @author Michael Jumper
  */
 public class UserContextService  {
-
-    /**
-     * Logger for this class.
-     */
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     /**
      * Service for accessing users.
@@ -60,42 +48,6 @@ public class UserContextService  {
      */
     @Inject
     private Provider<UserContext> userContextProvider;
-
-    /**
-     * The name of the HTTP password parameter to expect if the user is
-     * changing their expired password upon login.
-     */
-    private static final String NEW_PASSWORD_PARAMETER = "new-password";
-
-    /**
-     * The password field to provide the user when their password is expired
-     * and must be changed.
-     */
-    private static final Field NEW_PASSWORD = new Field(NEW_PASSWORD_PARAMETER, "New password", Field.Type.PASSWORD);
-
-    /**
-     * The name of the HTTP password confirmation parameter to expect if the
-     * user is changing their expired password upon login.
-     */
-    private static final String CONFIRM_NEW_PASSWORD_PARAMETER = "confirm-new-password";
-
-    /**
-     * The password confirmation field to provide the user when their password
-     * is expired and must be changed.
-     */
-    private static final Field CONFIRM_NEW_PASSWORD = new Field(CONFIRM_NEW_PASSWORD_PARAMETER, "Confirm new password", Field.Type.PASSWORD);
-
-    /**
-     * Information describing the expected credentials if a user's password is
-     * expired. If a user's password is expired, it must be changed during the
-     * login process.
-     */
-    private static final CredentialsInfo EXPIRED_PASSWORD = new CredentialsInfo(Arrays.asList(
-        CredentialsInfo.USERNAME,
-        CredentialsInfo.PASSWORD,
-        NEW_PASSWORD,
-        CONFIRM_NEW_PASSWORD
-    ));
 
     /**
      * Authenticates the user having the given credentials, returning a new
@@ -120,38 +72,7 @@ public class UserContextService  {
 
         // Authenticate user
         ModeledUser user = userService.retrieveUser(credentials);
-        if (user != null && !user.getModel().isDisabled()) {
-
-            // Update password if password is expired
-            if (user.getModel().isExpired()) {
-
-                // Pull new password from HTTP request
-                HttpServletRequest request = credentials.getRequest();
-                String newPassword = request.getParameter(NEW_PASSWORD_PARAMETER);
-                String confirmNewPassword = request.getParameter(CONFIRM_NEW_PASSWORD_PARAMETER);
-
-                // Require new password if account is expired
-                if (newPassword == null || confirmNewPassword == null) {
-                    logger.info("The password of user \"{}\" has expired and must be reset.", user.getIdentifier());
-                    throw new GuacamoleInsufficientCredentialsException("Password expired", EXPIRED_PASSWORD);
-                }
-
-                // New password must be different from old password
-                if (newPassword.equals(credentials.getPassword()))
-                    throw new GuacamoleClientException("LOGIN.ERROR_PASSWORD_SAME");
-
-                // New password must not be blank
-                if (newPassword.isEmpty())
-                    throw new GuacamoleClientException("LOGIN.ERROR_PASSWORD_BLANK");
-
-                // Confirm that the password was entered correctly twice
-                if (!newPassword.equals(confirmNewPassword))
-                    throw new GuacamoleClientException("LOGIN.ERROR_PASSWORD_MISMATCH");
-
-                // STUB: Change password if new password given
-                logger.info("Resetting expired password of user \"{}\".", user.getIdentifier());
-
-            }
+        if (user != null) {
 
             // Upon successful authentication, return new user context
             UserContext context = userContextProvider.get();
