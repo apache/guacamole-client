b23dcf83cac852aaa61b3d4b34e7da612fc449c4
Michael Jumper <mjumper@apache.org>
2016-08-20 13:54:20 -0700

    GUACAMOLE-77: Distinguish within REST between the user's identity/permissions and actual existing user objects.

--- guacamole/src/main/java/org/apache/guacamole/rest/user/UserDirectoryResource.java
+++ guacamole/src/main/java/org/apache/guacamole/rest/user/UserDirectoryResource.java
@@ -29,7 +29,6 @@ import org.apache.guacamole.GuacamoleException;
 import org.apache.guacamole.net.auth.User;
 import org.apache.guacamole.net.auth.Directory;
 import org.apache.guacamole.net.auth.UserContext;
-import org.apache.guacamole.rest.directory.DirectoryObjectResource;
 import org.apache.guacamole.rest.directory.DirectoryObjectResourceFactory;
 import org.apache.guacamole.rest.directory.DirectoryObjectTranslator;
 import org.apache.guacamole.rest.directory.DirectoryResource;
@@ -43,23 +42,6 @@ import org.apache.guacamole.rest.directory.DirectoryResource;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserDirectoryResource extends DirectoryResource<User, APIUser> {
-
-    /**
-     * The UserContext associated with the Directory which contains the
-     * User exposed by this resource.
-     */
-    private final UserContext userContext;
-
-    /**
-     * The Directory exposed by this resource.
-     */
-    private final Directory<User> directory;
-
-    /**
-     * A factory which can be used to create instances of resources representing
-     * Users.
-     */
-    private final DirectoryObjectResourceFactory<User, APIUser> resourceFactory;
 
     /**
      * Creates a new UserDirectoryResource which exposes the operations and
@@ -85,9 +67,6 @@ public class UserDirectoryResource extends DirectoryResource<User, APIUser> {
             DirectoryObjectTranslator<User, APIUser> translator,
             DirectoryObjectResourceFactory<User, APIUser> resourceFactory) {
         super(userContext, directory, translator, resourceFactory);
-        this.userContext = userContext;
-        this.directory = directory;
-        this.resourceFactory = resourceFactory;
     }
 
     @Override
@@ -98,18 +77,6 @@ public class UserDirectoryResource extends DirectoryResource<User, APIUser> {
             object.setPassword(UUID.randomUUID().toString());
 
         return super.createObject(object);
-
-    }
-
-    @Override
-    public DirectoryObjectResource<User, APIUser>
-        getObjectResource(String identifier) throws GuacamoleException {
-
-        // If username is own username, just use self - might not have query permissions
-        if (userContext.self().getIdentifier().equals(identifier))
-            return resourceFactory.create(userContext, directory, userContext.self());
-
-        return super.getObjectResource(identifier);
 
     }
 
