724a6a9737436b6e9a01eb209179e8ff34713758
Michael Jumper <mjumper@apache.org>
2017-08-26 17:52:53 -0700

    GUACAMOLE-210: Update API to 0.9.13-incubating.

--- extensions/guacamole-auth-openid/src/main/java/org/apache/guacamole/auth/openid/OpenIDAuthenticationProvider.java
+++ extensions/guacamole-auth-openid/src/main/java/org/apache/guacamole/auth/openid/OpenIDAuthenticationProvider.java
@@ -64,6 +64,11 @@ public class OpenIDAuthenticationProvider implements AuthenticationProvider {
     }
 
     @Override
+    public Object getResource() throws GuacamoleException {
+        return null;
+    }
+
+    @Override
     public AuthenticatedUser authenticateUser(Credentials credentials)
             throws GuacamoleException {
 
