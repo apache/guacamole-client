e1443d87b9955927394aa73323b4fe039ebf46f2
Nick Couchman <vnick@apache.org>
2018-01-03 13:07:24 -0500

    GUACAMOLE-237: Make standard comments instead of JSDoc comments.

--- guacamole-common-js/src/main/webapp/modules/AudioRecorder.js
+++ guacamole-common-js/src/main/webapp/modules/AudioRecorder.js
@@ -181,19 +181,15 @@ Guacamole.RawAudioRecorder = function RawAudioRecorder(stream, mimetype) {
      */
     var context = Guacamole.AudioContextFactory.getAudioContext();
 
-    /**
-     * Some browsers do not implement navigator.mediaDevices - this
-     * shims in this functionality to ensure code compatibility.
-     */
+    // Some browsers do not implement navigator.mediaDevices - this
+    // shims in this functionality to ensure code compatibility.
     if (!navigator.mediaDevices)
         navigator.mediaDevices = {};
 
-    /**
-     * Browsers that either do not implement navigator.mediaDevices
-     * at all or do not implement it completely need the getUserMedia
-     * method defined.  This shims in this function by detecting
-     * one of the supported legacy methods.
-     */
+    // Browsers that either do not implement navigator.mediaDevices
+    // at all or do not implement it completely need the getUserMedia
+    // method defined.  This shims in this function by detecting
+    // one of the supported legacy methods.
     if (!navigator.mediaDevices.getUserMedia)
         navigator.mediaDevices.getUserMedia = (navigator.getUserMedia
                 || navigator.webkitGetUserMedia
