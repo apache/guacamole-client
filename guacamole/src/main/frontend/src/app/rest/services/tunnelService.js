e5ba3b156c633dfabaa943d83f876038da960069
ares.li.harris <mli@harriscomputer.com>
2023-04-12 15:05:58 -0700

    Use "Guacamole-Token" header instead of the http parameter for /api/session/tunnels/<tunnel ID>/protocol?token=<token>

--- guacamole/src/main/frontend/src/app/rest/services/tunnelService.js
+++ guacamole/src/main/frontend/src/app/rest/services/tunnelService.js
@@ -97,17 +97,15 @@ angular.module('rest').factory('tunnelService', ['$injector',
      */
     service.getProtocol = function getProtocol(tunnel) {
 
-        // Build HTTP parameters set
-        var httpParameters = {
-            token : authenticationService.getCurrentToken()
-        };
-
         // Retrieve the protocol details of the specified tunnel
         return requestService({
             method  : 'GET',
             url     : 'api/session/tunnels/' + encodeURIComponent(tunnel)
                         + '/protocol',
-            params  : httpParameters
+            headers: {
+                'Guacamole-Token': authenticationService.getCurrentToken(),
+            }
+
         });
 
     };
