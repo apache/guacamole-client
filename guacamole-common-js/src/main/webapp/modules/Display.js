c1e49cb686bfdaf7d675b4b6910d5003ed9104b3
Corentin SORIANO <csoriano@apache.org>
2024-11-16 08:52:20 +0100

    GUACAMOLE-288: Adjust the display according to the number of monitors.

--- guacamole-common-js/src/main/webapp/modules/Display.js
+++ guacamole-common-js/src/main/webapp/modules/Display.js
@@ -38,6 +38,7 @@ Guacamole.Display = function() {
 
     var displayWidth = 0;
     var displayHeight = 0;
+    var displayMonitors = 1;
     var displayScale = 1;
 
     // Create display
@@ -551,6 +552,16 @@ Guacamole.Display = function() {
     };
 
     /**
+     * Returns the number of monitors.
+     * 
+     * @return {!number}
+     *     The number of monitors.
+     */
+    this.getMonitors = function getMonitors() {
+        return displayMonitors;
+    };
+
+    /**
      * Returns the default layer of this display. Each Guacamole display always
      * has at least one layer. Other layers can optionally be created within
      * this layer, but the default layer cannot be removed and is the absolute
@@ -740,7 +751,7 @@ Guacamole.Display = function() {
      * @param {!number} y
      *     The Y coordinate to move the cursor to.
      */
-    this.moveCursor = function(x, y) {
+    this.moveCursor = function moveCursor(x, y) {
 
         // Move cursor layer
         cursor.translate(x - guac_display.cursorHotspotX,
@@ -751,6 +762,16 @@ Guacamole.Display = function() {
         guac_display.cursorY = y;
 
     };
+
+    /**
+     * Change the number of monitors.
+     * 
+     * @param {!number} monitors
+     *     The number of monitors.
+     */
+    this.updateMonitors = function updateMonitors(monitors) {
+        displayMonitors = monitors;
+    }
 
     /**
      * Changes the size of the given Layer to the given width and height.
@@ -768,6 +789,9 @@ Guacamole.Display = function() {
      */
     this.resize = function(layer, width, height) {
         scheduleTask(function __display_resize() {
+
+            // Adjust width when using multiple monitors
+            width = width / displayMonitors;
 
             layer.resize(width, height);
 
