e99a5fe9ca258c8996de09c499a8503d1d842b89
Corentin SORIANO <csoriano@apache.org>
2024-11-16 08:52:20 +0100

    GUACAMOLE-288: Adjust the display according to the number of monitors.

--- guacamole/src/main/frontend/src/app/client/directives/guacClient.js
+++ guacamole/src/main/frontend/src/app/client/directives/guacClient.js
@@ -517,6 +517,7 @@ angular.module('client').directive('guacClient', [function guacClient() {
 
                 // Monitor count changed
                 if (display.getMonitors() !== monitors) {
+                    display.updateMonitors(monitors);
                     client.sendSize(width, height, monitors);
                 }
 
