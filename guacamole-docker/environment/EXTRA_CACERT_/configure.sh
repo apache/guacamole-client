0408351a12c45f7f4bec00afa6a2958c3eba4070
tiborauer <tibor.auer@gmail.com>
2025-03-06 16:06:02 +0000

    GUACAMOLE-1746: fix variable reference

--- guacamole-docker/environment/EXTRA_CACERT_/configure.sh
+++ guacamole-docker/environment/EXTRA_CACERT_/configure.sh
@@ -31,6 +31,6 @@
 ##
 CERT_PATH="EXTRA_CACERT_$(echo "searchPath" | sed 's/\([a-z]\)\([A-Z]\)/\1_\2/g' | tr 'a-z' 'A-Z')"
 
-for cert_fn in `find $CERT_PATH -name "*.crt"`; do
+for cert_fn in `find ${!CERT_PATH} -name "*.crt"`; do
     keytool -importcert -file $cert_fn -alias $(basename $cert_fn) -storepass changeit -noprompt -keystore $JAVA_HOME/lib/security/cacerts || true
 done
