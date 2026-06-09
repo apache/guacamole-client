0ba2118c44907622309dae0b511b8a80c58077e3
David Bateman <adb014@gmail.com>
2026-06-09 14:49:53 +0200

    GUACAMOLE-2137: Add wrapping of UserContext, so that User and ConnectionGroup information can be updated for the UI

---
+++ extensions/guacamole-vault/modules/guacamole-vault-hv/src/main/java/org/apache/guacamole/vault/hv/user/HvUserContext.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.guacamole.vault.hv.user;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import org.apache.guacamole.GuacamoleException;
+import org.apache.guacamole.form.Form;
+import org.apache.guacamole.net.auth.Connection;
+import org.apache.guacamole.net.auth.ConnectionGroup;
+import org.apache.guacamole.net.auth.DecoratingDirectory;
+import org.apache.guacamole.net.auth.DelegatingUserContext;
+import org.apache.guacamole.net.auth.Directory;
+import org.apache.guacamole.net.auth.User;
+import org.apache.guacamole.net.auth.UserContext;
+import org.apache.guacamole.net.auth.UserGroup;
+import org.apache.guacamole.vault.hv.conf.HvAttributeService;
+
+/**
+ * OpenBao/Hashicorp Vault-specific UserContext implementation which wraps the
+ * UserContext of some other extension, providing (or hiding) additional data.
+ */
+public class HvUserContext extends DelegatingUserContext {
+
+    /**
+     * Creates a new HvUserContext which wraps the given UserContext,
+     * providing (or hiding) additional Hv-specific data.
+     *
+     * @param userContext
+     *     The UserContext to wrap.
+     */
+    public HvUserContext(UserContext userContext) {
+        super(userContext);
+    }
+
+    @Override
+    public Directory<User> getUserDirectory() throws GuacamoleException {
+        return new DecoratingDirectory<User>(super.getUserDirectory()) {
+
+            @Override
+            protected User decorate(User object) {
+                return new HvUser(object);
+            }
+
+            @Override
+            protected User undecorate(User object) {
+                assert(object instanceof HvUser);
+                return ((HvUser) object).getUnderlyingUser();
+            }
+
+        };
+    }
+
+    @Override
+    public Collection<Form> getUserAttributes() {
+        Collection<Form> userAttrs = new HashSet<>(super.getUserAttributes());
+        userAttrs.addAll(HvAttributeService.HV_ATTRIBUTES);
+        return Collections.unmodifiableCollection(userAttrs);
+    }   
+    
+    @Override
+    public Directory<Connection> getConnectionDirectory() throws GuacamoleException {
+        return new DecoratingDirectory<Connection>(super.getConnectionDirectory()) {
+
+            @Override
+            protected Connection decorate(Connection object) throws GuacamoleException {
+                return new HvConnection(object);
+            }
+
+            @Override
+            protected Connection undecorate(Connection object) {
+                assert(object instanceof HvConnection);
+                return ((HvConnection) object).getUnderlyingConnection();
+            }
+
+        };
+    }
+    
+    @Override
+    public Collection<Form> getConnectionAttributes() {
+        Collection<Form> connectionAttrs = new HashSet<>(super.getConnectionAttributes());
+        connectionAttrs.addAll(HvAttributeService.HV_CONNECTION_ATTRIBUTES);
+        return Collections.unmodifiableCollection(connectionAttrs);
+    }
+
+    @Override
+    public Directory<ConnectionGroup> getConnectionGroupDirectory() throws GuacamoleException {
+        return new DecoratingDirectory<ConnectionGroup>(super.getConnectionGroupDirectory()) {
+
+            @Override
+            protected ConnectionGroup decorate(ConnectionGroup object) throws GuacamoleException {
+                return new HvConnectionGroup(object);
+            }
+
+            @Override
+            protected ConnectionGroup undecorate(ConnectionGroup object) {
+                assert(object instanceof HvConnectionGroup);
+                return ((HvConnectionGroup) object).getUnderlyingConnectionGroup();
+            }
+
+        };
+    }
+    
+    @Override
+    public Collection<Form> getConnectionGroupAttributes() {
+        Collection<Form> connectionGroupAttrs = new HashSet<>(super.getConnectionGroupAttributes());
+        connectionGroupAttrs.addAll(HvAttributeService.HV_ATTRIBUTES);
+        return Collections.unmodifiableCollection(connectionGroupAttrs);
+    }
+}
